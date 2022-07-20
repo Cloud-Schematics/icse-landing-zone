259a84d9ec87c3a9af54f87ff29ee3f58a887746
jvallexm <jvallexm@gmail.com>
2022-07-20 11:08:05 -0400

    Edge VPC no longer created by default

--- variables.f5.tf
+++ variables.f5.tf
@@ -11,7 +11,7 @@
 variable "add_edge_vpc" {
   description = "Create an edge VPC network and resource group. Conflicts with `create_edge_network_on_management_vpc`."
   type        = bool
-  default     = true
+  default     = false
 }
 
 variable "create_edge_network_on_management_vpc" {
